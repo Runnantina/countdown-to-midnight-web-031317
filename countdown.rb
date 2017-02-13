0e9b5d69029b93d6f25f9e0d1a526f4538c2c936
Runnan Zhong <r.tina.zhong@gmail.com>
2017-02-13 22:49:03 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
 
-def countdown
+def countdown(seconds_countdown)
+  while seconds_countdown > 0
+    puts "#{seconds_countdown} SECOND(S)!"
+    seconds_countdown -= 1
+  end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_countdown)
+  while seconds_countdown > 0
+    puts "#{seconds_countdown} SECOND(S)!"
+    sleep(1)
+    seconds_countdown -= 1
+  end
+    puts "HAPPY NEW YEAR!"
 end
